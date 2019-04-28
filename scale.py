cd01ca7e2024b90282492d81f5ab89e8336d0abc
Ekats3529 <45201820+Ekats3529@users.noreply.github.com>
2019-04-28 12:24:28 +0400

    Add files via upload

---
+++ scale.py
@@ -0,0 +1,12 @@
+import math
+
+def get_delta(a, b):
+
+    degree_to_meters_factor = 111
+    a_lon, a_lat = a
+    b_lon, b_lat = b
+
+    # Берем среднюю по широте точку и считаем коэффициент для нее.
+    radians_lattitude = math.radians((a_lat + b_lat) / 2.)
+    lat_lon_factor = math.cos(radians_lattitude)
+    return str(lat_lon_factor / 100)
---
+++ search.py
@@ -0,0 +1,70 @@
+import sys
+import requests
+import pygame
+import os
+from scale import get_delta
+
+# Пусть наше приложение предполагает запуск:
+# python search.py Москва, ул. Ак. Королева, 12
+# Тогда запрос к геокодеру формируется следующим образом:
+toponym_to_find = " ".join(sys.argv[1:])
+
+geocoder_api_server = "http://geocode-maps.yandex.ru/1.x/"
+
+geocoder_params = {"geocode": toponym_to_find, "format": "json"}
+
+response = requests.get(geocoder_api_server, params=geocoder_params)
+if not response:
+    # обработка ошибочной ситуации
+    pass
+
+# Преобразуем ответ в json-объект
+json_response = response.json()
+# Получаем первый топоним из ответа геокодера.
+toponym = json_response["response"]["GeoObjectCollection"]["featureMember"][0]["GeoObject"]
+# Координаты центра топонима:
+toponym_coodrinates = toponym["Point"]["pos"]
+toponym_lower = [float(x) for x in toponym['boundedBy']['Envelope']['lowerCorner'].split()]
+toponym_upper = [float(x) for x in toponym['boundedBy']['Envelope']['upperCorner'].split()]
+# Долгота и широта:
+toponym_longitude, toponym_lattitude = toponym_coodrinates.split(" ")
+
+
+delta = get_delta(toponym_lower, toponym_upper)
+
+# Собираем параметры для запроса к StaticMapsAPI:
+map_params = {
+    "ll": ",".join([toponym_longitude, toponym_lattitude]),
+    "spn": ",".join([delta, delta]),
+    "l": "map",
+    "pt": "{},pm2ntl".format(",".join([toponym_longitude, toponym_lattitude]))
+
+}
+
+map_api_server = "http://static-maps.yandex.ru/1.x/"
+# ... и выполняем запрос
+response = requests.get(map_api_server, params=map_params)
+
+
+# Запишем полученное изображение в файл.
+map_file = "map.png"
+try:
+    with open(map_file, "wb") as file:
+        file.write(response.content)
+except IOError as ex:
+    print("Ошибка записи временного файла:", ex)
+    sys.exit(2)
+
+# Инициализируем pygame
+pygame.init()
+screen = pygame.display.set_mode((600, 450))
+# Рисуем картинку, загружаемую из только что созданного файла.
+screen.blit(pygame.image.load(map_file), (0, 0))
+# Переключаем экран и ждем закрытия окна.
+pygame.display.flip()
+while pygame.event.wait().type != pygame.QUIT:
+    pass
+pygame.quit()
+
+# Удаляем за собой файл с изображением.
+os.remove(map_file)
